cc518e51465e5b78eed3526c25db0204f975c725
NincolP <98769695+NincolP@users.noreply.github.com>
2023-08-03 16:48:55 -0500

    Update MinStack.java

--- src/MinStack.java
+++ src/MinStack.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 
 public class MinStack {
 
-  //Create a stack TODO
+  //Create a stack TODO this is just the blueprint
   Stack<Integer> stack = new Stack<>();
     public MinStack() {
 
